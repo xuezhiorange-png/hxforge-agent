17e372c1109b588d66e3d3679d0182f3eb73101b
xuezhiorange-png <xuezhiorange@gmail.com>
2026-06-21 18:41:51 +0800

    feat(properties): expose complete public property API

--- src/hexagent/properties/__init__.py
+++ src/hexagent/properties/__init__.py
@@ -1,4 +1,30 @@
+from hexagent.properties.base import (
+    FluidIdentifier,
+    FluidState,
+    FluidValidationLevel,
+    PhaseRegion,
+    PropertyCacheInfo,
+    PropertyCacheKey,
+    PropertyProvenance,
+    PropertyProvider,
+    PropertyQueryType,
+    SaturationState,
+)
 from hexagent.properties.coolprop_provider import CoolPropProvider
 from hexagent.properties.errors import PropertyErrorCode, PropertyServiceError
 
-__all__ = ["CoolPropProvider", "PropertyErrorCode", "PropertyServiceError"]
+__all__ = [
+    "CoolPropProvider",
+    "FluidIdentifier",
+    "FluidState",
+    "FluidValidationLevel",
+    "PhaseRegion",
+    "PropertyCacheInfo",
+    "PropertyCacheKey",
+    "PropertyErrorCode",
+    "PropertyProvenance",
+    "PropertyProvider",
+    "PropertyQueryType",
+    "PropertyServiceError",
+    "SaturationState",
+]
