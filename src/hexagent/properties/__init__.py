37cbd1840e1b7c9e14b0f0bf696fc3a2dd0d6206
xuezhiorange-png <xuezhiorange@gmail.com>
2026-06-21 18:30:09 +0800

    fix(properties): export public provider API

---
+++ src/hexagent/properties/__init__.py
@@ -0,0 +1,4 @@
+from hexagent.properties.coolprop_provider import CoolPropProvider
+from hexagent.properties.errors import PropertyErrorCode, PropertyServiceError
+
+__all__ = ["CoolPropProvider", "PropertyErrorCode", "PropertyServiceError"]
