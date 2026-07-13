675d5e82cb89d7d2804229994b4d3c09e785594f
Charles Cheng <xuezhiorange@gmail.com>
2026-07-13 11:19:46 +0800

    test(task-021): format model tests

--- tests/exchangers/shell_tube/tube_layout/test_models.py
+++ tests/exchangers/shell_tube/tube_layout/test_models.py
@@ -4,7 +4,12 @@ import dataclasses
 
 import pytest
 
-from hexagent.exchangers.shell_tube.tube_layout.models import DEFERRED_CAPABILITIES, AxisOrientation, LatticeIndex, OriginMode
+from hexagent.exchangers.shell_tube.tube_layout.models import (
+    DEFERRED_CAPABILITIES,
+    AxisOrientation,
+    LatticeIndex,
+    OriginMode,
+)
 
 
 def test_value_objects_are_frozen() -> None:
@@ -14,7 +19,13 @@ def test_value_objects_are_frozen() -> None:
 
 
 def test_closed_enum_order_and_deferred_order() -> None:
-    assert [item.value for item in OriginMode] == ["CENTER_ON_LATTICE_POINT", "CENTER_ON_PRIMITIVE_CELL"]
-    assert [item.value for item in AxisOrientation] == ["PRIMARY_AXIS_X", "PRIMARY_AXIS_Y"]
+    assert [item.value for item in OriginMode] == [
+        "CENTER_ON_LATTICE_POINT",
+        "CENTER_ON_PRIMITIVE_CELL",
+    ]
+    assert [item.value for item in AxisOrientation] == [
+        "PRIMARY_AXIS_X",
+        "PRIMARY_AXIS_Y",
+    ]
     assert DEFERRED_CAPABILITIES[0] == "SHELL_DIAMETER_NOT_COMPUTABLE"
     assert DEFERRED_CAPABILITIES[-1] == "GOLDEN_VALIDATION_NOT_COMPUTABLE"
